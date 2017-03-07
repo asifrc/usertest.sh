7ce269b2c4f00a8caad16c4f069fec730cbd8280
Asif R Choudhury <choudhury.asif@gmail.com>
2017-03-06 22:40:28 -0600

    Add ability to type into terminal

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,9 +2,34 @@ var usertest = {
   "greeting": "Welcome to usertest.sh!",
   "question": "What is your favorite CLI tool?"
 };
+
 terminal = new Terminal({
   cursorBlink: true
 })
+
+var shellprompt = "$ ";
+
+terminal.prompt = function() {
+  terminal.write("\r\n" + shellprompt);
+};
+
 terminal.open(document.getElementById('terminal'));
 terminal.writeln(usertest.greeting);
-terminal.write(usertest.question);
+terminal.write(usertest.question + " ");
+
+terminal.on('key', function(key, ev) {
+  var printable = (
+    !ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey
+  );
+  if (ev.keyCode == 13) {
+          terminal.prompt();
+  }
+  else if (ev.keyCode == 8) {
+    if (terminal.x > shellprompt.length) {
+      terminal.write('\b \b');
+    }
+  }
+  else if (printable) {
+    terminal.write(key);
+  }
+});
